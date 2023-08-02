5ad96c8364681ae1f3cd94c7645b182384b53d33
Gonzalo Maggio <gonzalomaggio.fs@gmail.com>
2023-08-02 10:47:57 -0300

    Inicio de proyecto

---
+++ index.js
@@ -0,0 +1,11 @@
+/* let numeroA = 1;
+let numeroB = 2;
+let suma = numeroA + numeroB;
+console.log(suma); */
+
+const lista = prompt("Cuál es tu nombre?");
+if (lista == "juan") {
+  alert("Entra a la fiesta");
+} else {
+  alert("No entra a la fiesta");
+}
